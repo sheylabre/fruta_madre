0ced7dd910527a08d0db6eaaecca0df547ecfcb5
sheylabre <sheylabrenaicha@hotmail.com>
2016-12-25 07:30:52 -0500

    frutamadre

---
+++ static/js/app.js
@@ -0,0 +1,68 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+    items:5,
+    loop:true,
+    margin:10,
+    merge:true,
+    responsive:{
+        678:{
+            mergeFit:true
+        },
+        1000:{
+            mergeFit:false
+        }
+    }
+});
+
+/*
+$('#catalogue').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('#testimonials').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    singleItem: true,
+})
+
+*/
+
+
+
+
+
+
+
+
+
